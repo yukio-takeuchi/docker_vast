f7d3fc90876b640f4d9499584c6d93fccdfc3c8c
Yukio Takeuchi <yukio_takeuchi@icloud.com>
2020-02-01 15:49:11 +0900

    First commits of fromrocker

---
+++ fromrocker/rstudio/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker build . --tag yosemite0808/ubuntu_vast:latest
+# docker build . --tag yosemite0808/ubuntu_vast:$(date -d now +%Y%m%d)
+docker tag yosemite0808/ubuntu_vast:latest yosemite0808/ubuntu_vast:$(date -d now +%Y%m%d)
+
