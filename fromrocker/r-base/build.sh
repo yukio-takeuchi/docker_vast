fc88dd5e5658558533e3bbe6a6664297e8e1a930
Yukio Takeuchi <yukio_takeuchi@icloud.com>
2020-02-03 10:00:33 +0900

    As of 10AM 20200203

--- fromrocker/r-base/build.sh
+++ fromrocker/r-base/build.sh
@@ -2,5 +2,5 @@
 
 docker build . --tag yosemite0808/r_base_vast:latest
 # docker build . --tag yosemite0808/ubuntu_vast:$(date -d now +%Y%m%d)
-#docker tag yosemite0808/r_base_vast:latest yosemite0808/r_base_vast:$(date -d now +%Y%m%d)
+docker tag yosemite0808/r_base_vast:latest yosemite0808/r_base_vast:$(date -d now +%Y%m%d)
 
--- fromrocker/rstudio/build.sh
+++ fromrocker/rstudio/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build . --tag yosemite0808/ubuntu_vast:latest
+docker build . --tag yosemite0808/rstudio_vast:latest
 # docker build . --tag yosemite0808/ubuntu_vast:$(date -d now +%Y%m%d)
-docker tag yosemite0808/ubuntu_vast:latest yosemite0808/ubuntu_vast:$(date -d now +%Y%m%d)
+docker tag yosemite0808/rstudio_vast:latest yosemite0808/rstudio_vast:$(date -d now +%Y%m%d)
 
