18db768afc2f6d18f3738ece9edc8375c07f97eb
Yukio Takeuchi <yukio_takeuchi@icloud.com>
2020-02-04 13:24:40 +0900

    As of 13:24pm 20200204, set default command to R

--- official/r-base/build.sh
+++ official/r-base/build.sh
@@ -2,5 +2,5 @@
 
 docker build . --tag yosemite0808/r-base_fromofficial_vast:latest
 # docker build . --tag yosemite0808/ubuntu_vast:$(date -d now +%Y%m%d)
-docker tag yosemite0808/r-base_fromofficial_vast:latest yosemite0808/r-base_from_official_vast:$(date -d now +%Y%m%d)
+docker tag yosemite0808/r-base_fromofficial_vast:latest yosemite0808/r-base_fromofficial_vast:$(date -d now +%Y%m%d)
 
--- ubuntu/vast/build.sh
+++ ubuntu/vast/build.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-docker build . --tag yosemite0808/ubuntu_vast:latest
+if [ $# != 1 ]; then
+   echo "number of arguments differ from 1!"
+    exit 1
+fi
+
+LOGFILE=$1
+
+docker build . --tag yosemite0808/ubuntu_vast:latest | tee $1
 # docker build . --tag yosemite0808/ubuntu_vast:$(date -d now +%Y%m%d)
-docker tag yosemite0808/ubuntu_vast:latest yosemite0808/ubuntu_vast:$(date -d now +%Y%m%d)
+docker tag yosemite0808/ubuntu_vast:latest yosemite0808/ubuntu_vast:$(date -d now +%Y%m%d) | tee -a $1 
+if [ -f $1  ]; then
+  cp -p $1 ~/Dropbox/vast/log/
+fi 
 
