d914b283ca9dabedcf8c72eef5d67fd21c9c6eb3
Yukio Takeuchi <yukio_takeuchi@icloud.com>
2020-02-04 12:08:27 +0900

    As of 12:08pm 20200204 resoled warning when rgl is installed

--- official/r-base/build.sh
+++ official/r-base/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build . --tag yosemite0808/r-base_vast:latest
+docker build . --tag yosemite0808/r-base_fromofficial_vast:latest
 # docker build . --tag yosemite0808/ubuntu_vast:$(date -d now +%Y%m%d)
-docker tag yosemite0808/r-base_vast:latest yosemite0808/r_base_vast:$(date -d now +%Y%m%d)
+docker tag yosemite0808/r-base_fromofficial_vast:latest yosemite0808/r-base_from_official_vast:$(date -d now +%Y%m%d)
 
